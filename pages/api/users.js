4b287c2d088795fa5dcf33f2b1c424784ce9d398
MarkoNS1990 <marczecevic@gmail.com>
2021-08-27 13:58:55 +0200

    update api

--- pages/api/users.js
+++ pages/api/users.js
@@ -12,23 +12,24 @@ export function extractUser(filePath) {
 }
 
 export default function handler(req, res) {
-  if (req.method === "POST") {
-    const name = req.body.name;
-    const id = req.body.id;
+  //   if (req.method === "POST") {
+  //     const name = req.body.name;
+  //     const id = req.body.id;
 
-    const newUser = {
-      id: id,
-      name: name,
-    };
-    const filePath = buildUsersPath();
-    const data = extractUser(filePath);
-    data.push(newUser);
-    fs.writeFileSync(filePath, JSON.stringify(data));
-    res.status(201).json({ message: "Success", user: newUser });
-  } else {
-    const filePath = buildUsersPath();
-    const data = extractUser(filePath);
-    //res.status(200).json({ users: data });
-    res.status(200).json({ users: [{ name: "marko", id: "1" }] });
-  }
+  //     const newUser = {
+  //       id: id,
+  //       name: name,
+  //     };
+  //     const filePath = buildUsersPath();
+  //     const data = extractUser(filePath);
+  //     data.push(newUser);
+  //     fs.writeFileSync(filePath, JSON.stringify(data));
+  //     res.status(201).json({ message: "Success", user: newUser });
+  //   } else {
+  //     const filePath = buildUsersPath();
+  //     const data = extractUser(filePath);
+  //res.status(200).json({ users: data });
+
+  //}
+  res.status(200).json({ users: [{ name: "marko", id: "1" }] });
 }
